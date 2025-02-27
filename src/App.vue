7c0ff28674d66f7e95842e8039d1c0d9020b1961
Meruem117 <1572677319@qq.com>
2025-02-27 23:41:10 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,8 @@
 <template>
-    <v-app>
-        <v-app-bar>
-            <v-btn v-for="link in links" :key="link.path" @click="changeTab(link.path)">
-                {{ link.text }}
-            </v-btn>
-        </v-app-bar>
-        <v-main>
-            <router-view />
-        </v-main>
-    </v-app>
+  <div></div>
 </template>
 
-<script>
-import { ref } from 'vue'
-import router from './router/router'
-
-export default {
-    name: 'App',
-    setup() {
-        const links = ref([
-            { text: 'AMap', path: 'amap' },
-            { text: 'BaiduMap', path: 'bmap' },
-            { text: 'CesiumMap', path: 'cmap' },
-            { text: 'EchartsMap', path: 'emap' },
-        ])
-
-        return {
-            links
-        }
-    },
-    methods: {
-        changeTab(path) {
-            router.replace(path)
-        }
-    }
-}
+<script setup lang="ts">
 </script>
+
+<style scoped></style>
